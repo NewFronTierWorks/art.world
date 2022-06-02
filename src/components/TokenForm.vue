423221d5907764cbdd2734f0ebffa9e3d4c34fdc
NewFronTierWorks <nftw@tuta.io>
2022-06-02 12:59:50 -0600

    update schema

--- src/components/TokenForm.vue
+++ src/components/TokenForm.vue
@@ -5,6 +5,8 @@
             <v-card-text>
                 <v-text-field label="Title"></v-text-field>
                 <v-text-field label="Artist"></v-text-field>
+
+                {{formSchema}}
             </v-card-text>
         </v-card>
     </v-container>
@@ -12,31 +14,26 @@
 
 
 <script>
-import pinataSDK from '@pinata/sdk'
+import CdwaNftw from '@/assets/CDWA-NFTW-example.json'
 export default {
     name: 'TokenForm',
   components: {  },
   data () {
     return {
-        pinata: {}
+        CdwaNftw: {}
     }
   },
   mounted () {
-      this.pinataConnect();
   },
   methods: {
-      pinataConnect () {
-          const pinata = pinataSDK(process.env.PINATA_KEY, process.env.PINATA_SECRET)
-          pinata.testAuthentication().then((result) => {
-            //handle successful authentication here
-            console.log(result);
-        }).catch((err) => {
-            //handle error here
-            console.log(err);
-        });
-
-      }
+    
    
   },
+  computed: {
+      formSchema () {
+         return CdwaNftw
+      }
+
+  }
 }
 </script>
