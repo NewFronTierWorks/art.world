3189ed02c61bb301dccbbf189a2d064656691acb
NewFronTierWorks <nftw@tuta.io>
2022-06-13 14:14:04 -0600

    add wallet connect buttons

---
+++ src/components/ConnectWallet.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container class="fill-height" dark>
+    <v-row align="center" justify="center" class="fill-height">
+      <v-card class="pa-3 mt-12" width="400px">
+          <v-card-text>
+              <p>Connect your wallet to view your collection.</p>
+          </v-card-text>
+            <v-btn 
+            color="blue" 
+            class="my-3"
+            dark
+            block
+            @click="connectWallet"
+            
+            >
+                Connect Tezos Wallet
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn 
+            color="purple" 
+            class="my-3"
+            dark
+            block
+            @click="connectWallet"
+            
+            >
+                Connect Polygon Wallet
+            </v-btn>
+      </v-card>
+    </v-row>
+ </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'ConnectWallet',
+
+  data: () => ({
+  }),
+  methods: {
+      connectWallet ( ) {
+          console.log("Connect Wallet")
+          alert("Connect Wallet")
+      }
+  }
+}
+</script>
+
--- src/views/EntryWay.vue
+++ src/views/EntryWay.vue
@@ -1,18 +1,20 @@
 <template>
 <v-container>
-
   <start-menu-box />
+  <connect-wallet />
 </v-container>
 </template>
 
 <script>
   import StartMenuBox from '../components/StartMenuBox'
+  import ConnectWallet from '../components/ConnectWallet'
 
   export default {
     name: 'HomeView',
 
     components: {
-      StartMenuBox
+      StartMenuBox,
+      ConnectWallet
     },
   }
 </script>
