19f3ce037e68b5bb11f91d68c1e93fea008900d5
NewFronTierWorks <nftw@tuta.io>
2022-06-13 22:51:21 -0600

    add nftw

--- src/components/StartMenuBox.vue
+++ src/components/StartMenuBox.vue
@@ -3,7 +3,7 @@
     <v-row align="center" justify="center" class="fill-height">
       <v-card class="pa-3 mt-12" width="400px">
         <p>
-            <strong>NFTW</strong>-art.world allows museums, galleries and private collectors to tokenize art works using the CDWA standard, IPFS data storage and Tezos blockchain technology for ownership proofs.  
+            <strong>NFTW</strong>-art.world allows museums, galleries and private collectors to tokenize art works using the CDWA-<strong>NFTW</strong> standard, IPFS data storage and Tezos blockchain technology for ownership proofs.  
         </p>
         <v-card-actions>
             <v-btn 
