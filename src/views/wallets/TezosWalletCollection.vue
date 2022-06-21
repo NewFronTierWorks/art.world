576b1d72d62d9519d275848542f8f3ee9e280692
NewFronTierWorks <nftw@tuta.io>
2022-06-21 12:22:56 -0600

    Taquito connection

--- src/views/wallets/TezosWalletCollection.vue
+++ src/views/wallets/TezosWalletCollection.vue
@@ -4,55 +4,118 @@
       <v-card class="pa-3 mt-12" width="400px">
         <v-card-title>Tezos Wallet Collection</v-card-title>
         <v-card-subtitle></v-card-subtitle>
+        <v-card-text>
+          UserAddress: {{userAddress}}
+          <br>
+          Balance: {{balance}}
+
+        </v-card-text>
         <v-card-actions>
-            <v-btn 
-            color="blue" 
-            dark
-            block
-            >
-                Refresh
-            </v-btn>
-          
+          <v-btn color="blue" dark block @click="connectWallet">
+            Get Wallet
+          </v-btn>
+          <v-btn color="blue" dark block @click="fetchBalance">
+            Get Balance
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-row>
- </v-container>
+  </v-container>
 </template>
 
 <script>
 import { TezosToolkit } from '@taquito/taquito';
 
-import { TempleWallet } from '@temple-wallet/dapp';
+import { BeaconWallet } from '@taquito/beacon-wallet';
 
-const Tezos = new TezosToolkit('https://ithacanet.ecadinfra.com');
+import { TempleWallet } from '@temple-wallet/dapp'
+
+
+
+
 
 
 export default {
   name: 'TezosWalletCollection',
 
-  data: () => ({
-  }),
-  mounted () {
-    console.log(Tezos)
-    this.getTemple();
-    this.getWallet();
- 
+  data() {
+    return {
+      toolKit: {},
+      tezos: {},
+      wallet: {},
+      userAddress: '',
+      balance: 0,
+      errorMessage: '',
+      accountBalance: 0,
+      options: {
+        name: 'NFTW-art.World',
+        iconUrl: 'https://tezostaquito.io/img/favicon.svg',
+        preferredNetwork: 'ithacanet',
+        eventHandlers: {
+          PERMISSION_REQUEST_SUCCESS: {
+            handler: async (data) => {
+              console.log('permission data:', data);
+            },
+          },
+        },
+      }
+    }
+  },
+  mounted() {
+    this.getTezosToolkit();
+
   },
   methods: {
-    async getTemple () {
+    async connectWallet () {
       try {
         const available = await TempleWallet.isAvailable();
         if (!available) {
-          throw new Error('Thanos Wallet not installed');
+          throw new Error('Thanos Wallet not installed');          
         }
+        this.userAddress = '';
+        this.errorMessage = '';
+        this.wallet = new TempleWallet('NFTW-art.World');
+        await this.wallet.connect({
+          name: 'ithacanet',
+          rpc: 'https://ithacanet.ecadinfra.com'
+        });
+
+        //get Tezos singleton from Thanos Wallet
+        this.toolKit = await this.wallet.toTezos();
+        this.toolKit.setWalletProvider(this.wallet)
+
+        console.log("Wallet", this.wallet)
+        this.userAddress = await this.wallet.getPKH();
+
+
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     },
-    getWallet () {
-      const wallet = new TempleWallet('MyAwesomeDapp'); 
-      console.log(wallet)
-    }
+    async fetchBalance () {
+     this.accountBalance = await this.toolKit.tz.getBalance(this.userAddress)
+      console.log(this.accountBalance);
+    },
+    getTezosToolkit () {
+      this.tezos = new TezosToolkit('https://ithacanet.ecadinfra.com');
+    },
+    async getWallet() {
+      this.wallet = new BeaconWallet(this.options);
+      console.log(this.wallet)
+      console.log(this.tezos)
+      this.getBeacon();
+      // const userAddress = await this.wallet.getPKH();
+      // console.log(userAddress)
+      // this.tezos.setWalletProvider(this.wallet);
+    },
+    async getBeacon() {
+      await this.wallet.requestPermissions({
+        network: {
+          type: 'mainnet' | 'hangzhounet' | 'ithacanet' | 'custom',
+        },
+      });
+      
+    },
   }
 }
 </script>
